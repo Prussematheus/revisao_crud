db8a5f1c93d5865667ea0f5193548cbc0c8fecb1
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-11-03 08:59:26 -0300

    update - banco de dados

--- crud_revisao.sql
+++ crud_revisao.sql
@@ -1,1 +1,16 @@
-.
+create database crud_revisao;
+use crud_revisao;
+
+create table usuarios(
+id_usuario int not null auto_increment primary key,
+nome_usuario varchar(100) not null,
+email_usuario varchar(100)
+);
+
+create table tarefas(
+id_tarefa int not null auto_increment primary key,
+descricao_tarefa varchar (1000),
+prioridade_tarefa varchar (100),
+id_usuario int not null,
+foreign key (id_usuario) references usuarios (id_usuario)
+);
